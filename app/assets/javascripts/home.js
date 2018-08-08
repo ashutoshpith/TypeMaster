41584b743ffd959544fd05e01eb7d98bf8df21ae
ashutosh <amxdp18@gmail.com>
2018-08-08 19:07:20 +0530

    js implement error

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,7 +1,15 @@
 window.addEventListener('load', init)
-let time =5;
-let score = 0;
-let isPlaying;
+
+const levels = {
+  easy: 5,
+  medium: 3,
+  hard: 1
+}
+const currentLevel = levels.medium
+
+let time = currentLevel
+let score = 0
+let isPlaying
 
 let wordInput = documnet.querySelector('#word-input')
 
@@ -16,13 +24,89 @@ const words = [
   'hat',
   'river',
   'lucky',
-  'stubborn',
   'statue',
+  'generate',
+  'stubborn',
+  'cocktail',
+  'runaway',
   'joke',
+  'developer',
+  'establishment',
   'hero',
-  'echo'
+  'javascript',
+  'nutrition',
+  'revolver',
+  'echo',
+  'siblings',
+  'investigate',
+  'horrendous',
+  'symptom',
+  'laughter',
+  'magic',
+  'master',
+  'space',
+'definition'
 ]
 
 function init () {
-  console.log('init')
+  seconds.innerHTML = currentLevel
+  showWord(words)
+  wordInput.addEventListener('input', startMatch)
+  setInterval(countdown, 1000)
+  setInterval(checkStatus, 50)
+
+}
+
+function showWord (words) {
+  const randInex = Math.floor(Math.random() * words.length)
+  currentWord.innerHTML = words[randInex]
+  
+}
+
+function startMatch () {
+  if (matchWords()) {
+    isPlaying = true
+    time = currentLevel + 1
+    showWord(words)
+    wordInput.value = ''
+    score++  }
+
+  if (score == -1) {
+    scoreDisplay.innerHTML = 0
+  }else{
+    scoreDisplay.innerHTML = score
+  }
+  
+}
+
+function matchWords () {
+  if (wordInput.value == currentWord.innerHTML) {
+    message.innerHTML = 'Correct !!!'
+    return true
+    
+  }
+  else{
+    message.innerHTML = ''
+    return false
+  }
+  
+}
+
+function countdown () {
+  if (time > 0) {
+    time--
+  }else if (time == 0) {
+    isPlaying = false
+    
+  }
+timeDisplay.innerHTML = time  
+}
+
+function checkStatus () {
+  if (!isPlaying && time == 0) {
+    message.innerHTML = 'Game Over'
+    score = -1
+    
+  }
+  
 }
